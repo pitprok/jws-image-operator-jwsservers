2de7a588c8710e2b58d0236b87aba906d25bd664
Petros Marios Prokopiou <pprokopi@redhat.com>
2020-08-28 12:28:15 +0300

    Tried to replace Tomcat with JBossWebServer

--- pkg/apis/jwsservers/v1alpha1/register.go
+++ pkg/apis/jwsservers/v1alpha1/register.go
@@ -1,8 +1,8 @@
 // NOTE: Boilerplate only.  Ignore this file.
 
-// Package v1alpha1 contains API Schema definitions for the jws v1alpha1 API group
+// Package v1alpha1 contains API Schema definitions for the jwsservers v1alpha1 API group
 // +k8s:deepcopy-gen=package,register
-// +groupName=jws.apache.org
+// +groupName=jwsservers.web.servers.org
 package v1alpha1
 
 import (
